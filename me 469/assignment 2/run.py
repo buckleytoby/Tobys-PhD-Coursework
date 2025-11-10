8e6332c5300544255d48abb8eb71906151633933
Toby Buckley <buckley.toby@gmail.com>
2025-11-10 14:51:55 -0600

    making it work

--- me 469/assignment 2/run.py
+++ me 469/assignment 2/run.py
@@ -54,9 +54,9 @@ class Logger:
         d1 = self.logs[key]
         
         # forward-fill & backward-fill n zeros
-        d2 = np.zeros(n)
+        d2 = np.zeros([n] + list(d1.shape[1:]))
         
-        d3 = np.concatenate([d2, d1, d2])
+        d3 = np.concatenate([d2, d1, d2], axis=0)
         
         d4 = []
         for idx in range(n, len(d3)-n):
@@ -65,8 +65,13 @@ class Logger:
             
             d4.append(m)
 
+        d5 = np.array(d4)
+
+        # remove first and last n
+        d6 = d5[n:-n]
+
         f = plt.figure()
-        plt.plot(d4)
+        plt.plot(d6)
         plt.title(key)
 
         plt.show()
@@ -82,7 +87,7 @@ class Logger:
             
             v2 = np.mean(v1)
             
-            print("{} avg last n: {}".format(key, v2))
+            print("{} avg last {}: {}".format(key, n, v2))
 
 # global
 LOG = True
@@ -129,11 +134,14 @@ class Layer:
             # old value
             p = self.params[key]
 
-            # new val
-            assert(p.shape == grad.shape)
-            assert(not np.any(np.isnan(grad)))
+            # mean the grads along the batch dim. Mean instead of sum so it doesn't scale with batch size
+            g2 = np.mean(grad, axis=0, keepdims=True)
 
-            new_p = p + grad
+            # new val
+            assert(p.shape == g2.shape)
+            assert(not np.any(np.isnan(g2)))
+
+            new_p = p + g2
 
             self.params[key] = new_p
 
@@ -203,18 +211,24 @@ class Dense(Layer):
         return dy/dx * upstream_grad (chain rule)
         """
         # we expect upstream_grad to be [n, 1] and self.x.T to be [1, m] and self.grads['A'] to be [n, m]
-        self.grads['A'] = upstream_grad @ self.x.T
+        x2 = np.permute_dims(self.x, [0, 2, 1])
+        self.grads['A'] = upstream_grad @ x2
+        # self.grads['A'] = self.x @ upstream_grad
         
         self.grads['B'] = upstream_grad
 
 
-        assert(self.grads['A'].shape == self.params['A'].shape)
-        assert(self.grads['B'].shape == self.params['B'].shape)
+        assert(self.grads['A'].shape[1:] == self.params['A'].shape[1:])
+        assert(self.grads['B'].shape[1:] == self.params['B'].shape[1:])
         assert(not np.any(np.isnan(self.grads['A'])))
         assert(not np.any(np.isnan(self.grads['B'])))
 
-        out = upstream_grad.T @ self.params['A']
-        return out.T
+        upstream_grad2 = np.permute_dims(upstream_grad, [0, 2, 1])
+
+        out = upstream_grad2 @ self.params['A']
+
+        out2 = np.permute_dims(out, [0, 2, 1])
+        return out2
 
 class LeakyReLu(Layer):
     """
@@ -261,6 +275,7 @@ class NN:
     neural net class
     """
     def __init__(self, input, hidden, output) -> None:
+
         # simple MLP
         self.dense1 = Dense(input, hidden)
         self.nonlinear1 = LeakyReLu(scale = 0.1)
@@ -404,14 +419,19 @@ class QLearning:
         dy = target_q - qval2
         loss = 0.5 * (dy.T @ dy)
 
+        # add the var dim back in
+        dy2 = np.expand_dims(dy, dy.ndim)
+
         # assuming MSE loss, the gradient of params p is dL/dp = (y' - y') * dy/dp -> dL/dy = (y' - y) * -1
         # dL/dy = (y' - y) * -1
-        self.dLdy = -dy
+        self.dLdy = -dy2
 
         # test
         if np.any(np.abs(loss) > 100.0):
             pass
 
+        # logging
+        logger.log_one("qval", qval.mean())
         return loss
     
     def backward(self, upstream_grad = None):
@@ -421,7 +441,9 @@ class QLearning:
         this is for the CRITIC network only
         """
         assert(upstream_grad is not None)
-        g = np.reshape(upstream_grad, [1, 1])
+        # g = np.reshape(upstream_grad, [1, 1])
+        g = upstream_grad
+        assert(g.ndim == 3)
         return self.Q.backward(g)
 
     def step(self, state, action, reward, statep, done):
@@ -469,12 +491,17 @@ class Optimizer:
         """
         use actor to get action, use critic to evaluate the action, then propagate it all backwards
         """
+        # forward pass through the actor
         action = self.policy.forward(state)
 
+        # must do the forward pass to save state values
         qval = self.critic.forward(state, action)
 
+        # logging
+        logger.log_one("actor_qval", qval.mean())
+
         # now we want to maximize the qval, so L = -qval, and therefore dL/dqval = -1.0
-        dL_dqval = -1.0
+        dL_dqval = -1.0 * np.ones_like(qval)
 
         # we want to min the loss, so negate the grad
         g = -dL_dqval
@@ -484,7 +511,7 @@ class Optimizer:
         assert(dC_dqval is not None)
 
         # first `state_dim` entries pertain to the state, last `action_dim` elements are the action grads
-        g = dC_dqval[self.state_dim:]
+        g = dC_dqval[:, self.state_dim:]
 
         # backprop through the actor
         self.policy.backward(g)
@@ -611,7 +638,7 @@ class Env:
             # reward for reaching the goal
             d = self.G.dist(c)
             if d < eps:
-                reward += 10.0
+                reward += 1000.0
 
                 # reached goal means we're done
                 print("Reached goal")
@@ -621,6 +648,9 @@ class Env:
         if self.nb_steps > self.max_nb_steps:
             print("Timeout")
             done = True
+
+        # normalize rewards to [-1, 1]
+        reward /= 1000.0
 
         return reward, done
 
@@ -690,6 +720,14 @@ class Training:
             v = val / l
             
             print("{} avg: {}".format(key, v))
+            logger.log_one("avg_{}".format(key), v)
+
+    def ep_summer(self, dd):
+        for key, val in dd.items():
+            v = np.sum(val)
+            
+            print("{} total: {}".format(key, v))
+            logger.log_one("total_{}".format(key), v)
 
     def run(self, nb_episodes):
         # initial values
@@ -701,19 +739,26 @@ class Training:
         done = False
         ep_averager = defaultdict(float)
         c = 0
+
+        # goal conditioned
+
         
         def reset():
             nonlocal ep_averager, c, nb_episodes, state, done
             
             ep_len = ep_averager['len']
-            logger.print_avg_last_n(ep_len)
+
+            print("------------------------------------------------")
             print("Ep reset. #{} of {}".format(c, nb_episodes))
+            logger.print_avg_last_n(ep_len)
             self.avg_and_print(ep_averager)
+            self.ep_summer(ep_averager)
             print("------------------------------------------------")
             
             state = self.env.reset()
             done = False
             ep_averager = defaultdict(float)
+
 
         while c < nb_episodes:
             # whether to reset
@@ -722,7 +767,7 @@ class Training:
                 reset()
 
             # query policy for an action
-            # action = self.policy.forward(state)
+            action = self.policy.forward(state)
 
             # step env
             statep, reward, done = self.env.step(action)
@@ -757,13 +802,13 @@ def main():
     nb_hidden_layers = 2
 
     # learning params
-    lra = 1e-5
-    lrc = 1e-4
+    lra = 2.5e-6
+    lrc = 2.5e-6
     gamma = 0.95 # discount
     nb_epochs = 1000
-    batch_size = 32
+    batch_size = 128
     max_nb_episode_steps = 100 # recall, each step is 0.1 seconds
-    nb_episodes = 1
+    nb_episodes = 1000
 
     # env params
     state_dim = 5 # [x, y, th, xg, yg]
@@ -787,9 +832,11 @@ def main():
     #     pass
 
     # plotting
-    n = 25
+    n = 100
     logger.plot_avg('qloss', n)
     logger.plot_avg('reward', n)
+    logger.plot_avg("qval", n)
+    logger.plot_avg("total_reward", 10)
 
     input("press any key to exit")
 
